c5a82af80bdcb375aa1e9bc5f10b544dd6d9e1b5
John Guinn <44623156+jguinn124@users.noreply.github.com>
2021-12-02 22:16:11 -0600

    handleChoice function

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //shuffle cards
   const shuffleCards = () => {
@@ -25,7 +27,9 @@ function App() {
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  const handleChoice = (card) => {
+    console.log(card);
+  };
 
   return (
     <div className="App">
@@ -33,7 +37,7 @@ function App() {
       <button onClick={shuffleCards}> New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <SingleCard key={card.id} card={card} />
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import React from "react";
 import "./SingleCard.css";
 
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice }) => {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="front" />
-        <img className="back" src="/img/cover.png" alt="back" />
+        <img
+          className="back"
+          onClick={handleClick}
+          src="/img/cover.png"
+          alt="back"
+        />
       </div>
     </div>
   );
